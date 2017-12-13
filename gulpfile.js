16e42fb372490d9ad5d8e6b199e421a4545a14f3
theren0406 <km0299.tw@yahoo.com.tw>
2017-12-13 22:52:26 +0800

    Completed large hero block and intro content

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+// var gulp = require('gulp');
+
+// gulp.task('default', function(){
+// 	console.log('I created a gulp task');
+// });
+
+
+

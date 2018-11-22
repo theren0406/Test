54e1373d977c22f7e4d3e27454ff28b3b8b89b1f
Cindy Chen <cindychen@gomaji.com>
2018-11-22 15:54:55 +0800

    Cleaned homework files

--- React/Homework/src/components/App.js
+++ React/Homework/src/components/App.js
@@ -1,13 +1,9 @@
 import React, { Component, Fragment } from 'react';
 
-import Typer from './Typer';
-
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Typer />
-      </div>
+      <div>For Homework</div>
     );
   }
 }
--- React/Homework/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button(props) {
-  return (
-    <button className="btn" onClick={props.click}>
-      {props.content}
-    </button>
-  )
-}
